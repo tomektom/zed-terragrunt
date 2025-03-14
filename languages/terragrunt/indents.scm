0f2fd69896992a0749f94075fe7778a6daec9dbd
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2025-03-14 13:06:03 +0100

    experiment

--- languages/terragrunt/indents.scm
+++ languages/terragrunt/indents.scm
@@ -10,7 +10,6 @@
   "]"
   "}"
   ")"
-  (heredoc_template)
 ] @outdent
 
 ; https://github.com/nvim-treesitter/nvim-treesitter/blob/ce4adf11cfe36fc5b0e5bcdce0c7c6e8fbc9798a/queries/terraform/indents.scm
